47fff0b9f89fd58d2b771271985a9427b290eced
Ashiq Emran <Ashiq Emran@DESKTOP-J2JE36M>
2017-09-16 10:18:50 -0700

    Angular 4 Basic Routing Completed

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,41 +1,18 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
-  
-   getHeroes(): void {
-     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-   }
-  
-   ngOnInit(): void {
-     this.getHeroes();
-   }
-   heroes: Hero[];
-  
-  providers: [HeroService];
-  title = 'app';
-  hero: Hero = {
-    id: 1,
-    name: "Ashiqur Rahman Emran"
-  };
-  
-  selectedHero=new Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    
-  }
-  
-  
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
 
-export class Hero {
-  id: number;
-  name: string;
-}
